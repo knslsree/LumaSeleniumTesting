02a39800eadb32994a88a669373b1fcd85925963
Shilpa <knsl.sree@gmail.com>
2023-05-16 09:59:05 +0200

    My Project

---
+++ Test/shilpika.js
@@ -0,0 +1,95 @@
+// Includes
+const {
+    Builder,
+    By,
+    Key,
+    until
+} = require('selenium-webdriver');
+const should = require('chai').should();
+
+/*  As a customer,
+I want to be able to search for a product of particular color,
+so that i can find the product that matches my preferences or needs.
+*/
+
+//Test grouping:search
+describe('Search for a product of particular color', () => {
+    //Test case:
+    context('Given a user is on the search page', () => {
+        it('should show the shirt of preferred color', async () => {
+            //start the webbrowser
+            const driver = await new Builder().forBrowser('firefox').build();
+
+            //Search for a product
+            try {
+                //Move to magento site
+                await driver.get('https://magento.softwaretestingboard.com/');
+                //Get the search input
+                await driver.wait(until.elementLocated(By.css('#search')), 50000);
+                await driver.findElement(By.id('search')).sendKeys('shirt for men', Key.RETURN);
+
+                // Scroll down a bit
+                await driver.wait(until.elementsLocated(By.css('.item.product.product-item')), 50000);
+                await driver.executeScript('document.querySelector(".item.product.product-item").scrollIntoView();');
+
+                //Find the first product
+                await driver.wait(until.elementLocated(By.css('.item.product.product-item:first-child')), 50000);
+
+                const product = await driver.findElement(By.css('.item.product.product-item:first-child'));
+
+
+
+
+                //find the information in the product we selected
+
+                await driver.wait(until.elementLocated(By.css('.swatch-attribute.color')), 50000);
+                let swatchColors = await product.findElement(By.css('.swatch-attribute.color'));
+
+                let productColor1 = await swatchColors.findElement(By.css('div[index="1"]'));
+                productColor1.click();
+                console.log("Choose orange color shirt");
+
+
+                productColor2 = await swatchColors.findElement(By.css('div[index="2"]'));
+                await driver.sleep(5000);
+                setTimeout(() => {
+                    productColor2.click().then(() => {
+                        console.log("Choose purple color shirt");
+                    });
+                }, 5000);
+
+
+
+                productColor3 = await swatchColors.findElement(By.css('div[index="0"]'));
+                await driver.sleep(5000);
+                setTimeout(() => {
+                    productColor3.click().then(() => {
+                        console.log("Choose blue color shirt");
+                    });
+                }, 5000);
+                await driver.sleep(10000);
+                //Asserts
+                assert.equal(productColor1, 'Orange'); //Builtin Node
+                expect(productColor1).to.equal('Orange'); // Chai expect
+                productColor1.should.equal('Orange'); // Chai should
+
+                assert.equal(productColor2, 'Purple'); //Builtin Node
+                expect(productColor2).to.equal('Purple'); // Chai expect
+                productColor2.should.equal('Purple'); // Chai should
+
+                assert.equal(productColor3, 'Blue'); //Builtin Node
+                expect(productColor3).to.equal('Blue'); // Chai expect
+                productColor3.should.equal('Blue'); // Chai should
+
+
+
+
+            } catch (Exception) {
+
+            } finally {
+                await driver.quit();
+            }
+
+        });
+    });
+});
